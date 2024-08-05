ca811ce67c547723e66d5797d305fdb0866abf18
Rabinagurung <rabinagurung347@gmail.com>
2024-08-05 01:36:34 -0400

    check firebase config

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+export const fbConfig = {
+  apiKey: 'AIzaSyAj6j4TWNCVrfuwWJYbJOA-fJDsy51sdWQ',
+  authDomain: 'r-n-lab-2.firebaseapp.com',
+  projectId: 'r-n-lab-2',
+  storageBucket: 'r-n-lab-2.appspot.com',
+  messagingSenderId: '723102479780',
+  appId: '1:723102479780:web:879b53cf2fce1228c2fd31'
+};
